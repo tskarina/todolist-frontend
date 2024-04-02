9eae4f1c839ee6b7f5703384e65a7914f0004fce
Karina Sodré <stkarinasodre@gmail.com>
2024-04-02 01:37:33 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
 
@@ -9,6 +14,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
         </Routes>
